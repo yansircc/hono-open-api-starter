53ef2d5778b786c9c62e20e5ea6ab287bfb7ce08
w3cj <cj@null.computer>
2024-10-24 15:31:34 -0600

    chore: upgrade drizzle to v0.35

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,3 @@
-import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 import type { Environment } from "@/env";
@@ -6,14 +5,14 @@ import type { Environment } from "@/env";
 import * as schema from "./schema";
 
 export function createDb(env: Environment) {
-  const client = createClient({
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
-  });
-
-  const db = drizzle(client, {
+  const db = drizzle({
+    connection: {
+      url: env.DATABASE_URL,
+      authToken: env.DATABASE_AUTH_TOKEN,
+    },
+    casing: "snake_case",
     schema,
   });
 
-  return { db, client };
+  return { db };
 }
