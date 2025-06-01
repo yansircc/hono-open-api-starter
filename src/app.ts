26ff4d6dbf07bdb26de098036ccc885650811efa
Penn York <markyan@foxmail.com>
2025-06-02 02:48:30 +0800

    :sparkles: add CORS middleware to enhance API security and accessibility

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import createApp from "@/lib/create-app";
 import envInfo from "@/routes/env-info/env.index";
 import index from "@/routes/index.route";
 import tasks from "@/routes/tasks/tasks.index";
+import { cors } from "hono/cors";
 
 const app = createApp();
 
 configureOpenAPI(app);
+
+app.use("*", cors());
 
 const routes = [
   index,
